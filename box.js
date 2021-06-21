e882c4ab8ff9131f72b1283cb77c4bcbc92ad5e2
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-06-21 17:10:40 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,20 @@
+class Box
+  {
+    constructor()
+    {
+      this.x =100;
+      this.y = 200
+      this.w = 50;
+      this.h = 50;
+    }
+    
+    show()
+    {
+      rect(this.x,this.y,this.w,this.h)
+    }
+    
+    //write function to set the width of the box
+
+  }
+
+  
