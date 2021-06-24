cdb61e5b16dbc94b984e6a3ba09aa27fd721d7e8
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-06-24 11:53:28 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,13 @@
 
 var box1;
-var box2;
 
 function setup() {
   createCanvas(600, 400);
-  box1 = new Box();
-  box2 = new Box();
+  
 }
 
 function draw() {
   background(220);
-  box1.show();
 
 }
 
